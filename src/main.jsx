1215ec0468573618201f54617b31fcf215f4f515
naye2m <naye2m@outlook.com>
2024-09-09 11:28:15 +0600

    chenges to react apps

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,25 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
+
+// ? : user defined js scripts
+// window.addEventListener("load",function(){})
+// import './assets/js/jquery.min.js';
+// import './assets/js/jquery.easing.1.3.js';
+// import './assets/js/bootstrap.min.js';
+// import './assets/js/jquery.waypoints.min.js';
+// import './assets/js/jquery.stellar.min.js';
+// import './assets/js/jquery.easypiechart.min.js';
+// // import './assets/js/google_map.js';
+// import './assets/js/main.js';
+// // import './assets/js/naye2m.js';
+// import './assets/js/modernizr-2.6.2.min.js';
+// import './assets/js/respond.min.js';
+
+
